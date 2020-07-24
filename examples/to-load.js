dcc3404db3704cb49a70104eeb15e970273ec6f1
Maxim Zimaliev <zimaliev@gmail.com>
2020-07-24 22:57:30 +0300

    namespace introduction

--- examples/to-load.js
+++ examples/to-load.js
@@ -1,9 +1,11 @@
+const Ref = require("../lib/ref.js");
+
 module.exports = function(s, w, v) {
-  var v0 = v.set("insert142", function(s, w, v) {
+  var v0 = v.set(Ref.parse("insert142"), function(s, w, v) {
     return [s, w.cons("to-load-words mark"), v];
   });
 
-  var [s1, w1, v1] = v0.get(":file")(s, w, v0);
+  var [s1, w1, v1] = v0.get(Ref.parse(":file"))(s, w, v0);
 
   return [ s1.cons(184).cons(934), w1.cons(132), v1 ];
 };
